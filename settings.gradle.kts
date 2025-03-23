b2199d6dce14ad97aab59dd75d7466db95a1b740
Miroslav Oujeský <oujesky@users.noreply.github.com>
2025-03-23 13:14:09 +0100

    Allow overriding H2 version for builds

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,3 +5,14 @@ include("h2-loader")
 include("h2-test")
 include("h2-mariadb-test")
 include("h2-mariadb-functions")
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            startParameter.projectProperties["h2.version"]?.let {
+                logger.lifecycle("Overriding H2 version: {}", it)
+                version("h2", it)
+            }
+        }
+    }
+}
